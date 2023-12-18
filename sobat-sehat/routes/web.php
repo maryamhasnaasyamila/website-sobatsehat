751727ad3d80c177d62033ca8600cc74f2c07169
Maryam Hasnaa' Syamila <hasyamil4@gmail.com>
2023-12-18 10:53:46 +0700

    update

--- sobat-sehat/routes/web.php
+++ sobat-sehat/routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -16,5 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', 
+[DashboardController::class, 'index']);
 
 
